52f4f9e9dd558a83fc945bf175479a09dddb2072
Krevor <Rakrevor@gmail.com>
2026-08-25 13:02:27 +0300

    finnished module 3, exercise 6

--- Software1/module 3/excersise 6.py
+++ Software1/module 3/excersise 6.py
@@ -1,2 +1,14 @@
 import random
 
+num1 = random.randint(0, 9)
+num2 = random.randint(0, 9)
+num3 = random.randint(0, 9)
+
+num4 = random.randint(1, 6)
+num5 = random.randint(1, 6)
+num6 = random.randint(1, 6)
+num7 = random.randint(1, 6)
+
+
+print(f"3-digit code: {num1}{num2}{num3}")
+print(f"4-digit code: {num4}{num5}{num6}{num7}")
