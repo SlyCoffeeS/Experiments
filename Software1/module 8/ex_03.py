d4dbe5ecc3689f26454860db0a6cee9668b4360b
Krevor <Rakrevor@gmail.com>
2026-09-09 20:02:46 +0300

    fixed nodule 8 ex 03

--- Software1/module 8/ex_03.py
+++ Software1/module 8/ex_03.py
@@ -1,30 +1,31 @@
-Airport_data_managment = {}
+airports = {}
 
 while True:
 
-    print("Airport Data Managment")
+    print("\nAirport Data Management")
     print(f"1. Enter a new airport")
     print(f"2. Fetch airport information")
     print(f"3. Quit")
 
-    command = input(f"Please choose an option (1-3)")
+    command = input(f"Please choose an option (1-3): ")
 
     if command == "1":
-        ICAO_code = input("Enter ICAO code: ")
-        airport_name = input("Enter airport name: ")
-        Airport_data_managment[ICAO_code] = airport_name
+        ICAO_code = input("Enter the ICAO code: ")
+        airport_name = input("Enter the airport name: ")
+        print(f"Airport {airport_name} with ICAO code {ICAO_code} has been added.")
+        airports[ICAO_code] = airport_name
 
     elif command == "2":
 
-        ICAO_code = input("Enter ICAO code")
+        ICAO_code = input("Enter the ICAO code: ")
 
-        if ICAO_code in Airport_data_managment:
-                print(f"Airport name: {Airport_data_managment[ICAO_code]}")
+        if ICAO_code in airports:
+                print(f"The airport with ICAO code {ICAO_code} is {airport_name}.")
         else:
-                print(f"Code {ICAO_code} not found")
+                print(f"No airport found with ICAO code {ICAO_code}.")
 
     elif command == "3":
-        print("Thank you for using the Airport Data Managment system. Goodbye!")
+        print("Thank you for using the Airport Data Management system. Goodbye!")
         break
 
     else:
