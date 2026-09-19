8b7630a9d78e2ce778075e041383c16c7f23f5fc
Krevor <Rakrevor@gmail.com>
2026-09-19 15:12:04 +0300

    module 10 unfinished files

---
+++ Software1/module 10/ex_01.py
@@ -0,0 +1,41 @@
+class Elevator:
+    def __init__(self, bottom, top):
+        self.bottom_floor
+        self.top_floor
+        self.current_floor = 0
+
+    def go_to_floor(self, floor):
+        while self.bottom_floor != self.go_to_floor:
+            if self.go_to_floor > self.bottom_floor:
+                for f in floor:
+                    self.floor_up
+                    print(self.bottom_floor)
+                    return
+                
+            elif self.go_to_floor < self.bottom_floor:
+                for f in floor:
+                    self.floor_down
+                    print(self.bottom_floor)
+                    return
+        
+    def floor_up(self):
+        if self.current_floor < self.top_floor or != self.go_to_floor:
+            self.current_floor + 1
+            print(self.bottom_floor)
+
+    def floor_down(self):
+        if self.bottom_floor != 0:
+            self.bottom_floor - 1
+            print(self.bottom_floor)
+
+                
+    H = Elevator(0, 5)
+
+
+
+
+
+
+
+        
+
