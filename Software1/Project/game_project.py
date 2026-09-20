7442cb84b10a698ef713de59dc1941a8cd6be079
SlyCoffeeS <rkrevor@gmail.com>
2026-09-20 17:46:33 +0300

    finnished project assignemnt 3 and updated readme file for it.

--- Software1/Project/game_project.py
+++ Software1/Project/game_project.py
@@ -1,31 +1,68 @@
 ## crazy to start making a game before knowing what the game -
 ## - is going to be even about
+inventory = []
+
+def add_item():
+    what = input("What would you like to add to the inventory?")
+
+    inventory.append(what)
+    print(f"{what} has been added to inventory")
+    
+def open_inventory():
+
+    print("Inventory contains")
+
+    for item in inventory:
+        print("- " + item)
+
+def drop_item():
+    drop = input("What item would you like to drop?")
+
+    if drop in inventory:
+
+        inventory.remove(drop)
+        print(f"{drop} has been dropped on the floor.")
+
+    else:
+        print("cant drop something thats not there!")
+
+
+
+def main_menu():
+    while True:
+        print("\n Main menu ")
+        print("1. Add item")
+        print("2. Open inventory")
+        print("3. Drop item")
+        print("4. Lopeta")
+        print("\n Fun menu")
+        print("5. Greet")
+        print("6. count")
+
+        command = input("What would you like to do?")
+
+        if command == "1":
+            add_item()
+        elif command == "2":
+            open_inventory()
+        elif command == "3":
+            drop_item()
+        elif command == "4":
+            print("catch ya later")
+            break
+        elif command == "5":
+            print(f"Tere!, kuidas laheb?")
+        
+        elif command == "6":
+            print("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
 
 name=input("Insert your name: ")
 age=int(input("insert your age: "))
 print(name)
 print(age)
 
-menu = "select option\n1. greet\n2. count\n3. lopeta."
-
-if age < 12:
-    print ("user is a minor, Come back when older")
-elif age >= 12:
+if age >= 12:
     print(f"Welcome {name}!")
-command = input(f"What shall we do now? {menu}")
-
-
-while age >= 12:
-    if command == "lopeta":
-        break
-
-    elif command == "greet":
-        print(f"Tere!, kuidas laheb?")
-        
-
-    elif command == "count":
-        print("1, 2, 3, 4, 5, 6, 7, 8, 9, 10")
-
-    select = input(menu)
-    command = input("Anything else you would like to do?")
-
+    main_menu()
+else:
+    print ("user is a minor, Come back when older")
--- Software1/Project/readme.md
+++ Software1/Project/readme.md
@@ -1,2 +1,6 @@
 ## Farm of Something
 Krevor Raudseppf
+
+havent been updating this, so a quick recap! 
+
+so far i have coded in a age check, then made a simple menu, and now third project assigenment i implemented functions for main menu, while implementing functions i decided to make "main menu" into a functions also. (was a while if else print before, now its similar, but made it a function. so its easier to use in the future.) but we regress. so now i can add items, drop items, check inventory. and also theres the fun menu that i had to make for project assignment 2.
